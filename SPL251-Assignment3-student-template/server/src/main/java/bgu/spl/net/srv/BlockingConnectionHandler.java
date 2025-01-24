0f6aa0ad1fdb276f62430bc855ddc6c925193813
alonganot10 <alonganot@gmail.com>
2025-01-24 12:19:04 +0000

    implemented ConnectionHandler

--- SPL251-Assignment3-student-template/server/src/main/java/bgu/spl/net/srv/BlockingConnectionHandler.java
+++ SPL251-Assignment3-student-template/server/src/main/java/bgu/spl/net/srv/BlockingConnectionHandler.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package bgu.spl.net.srv;
 
 import bgu.spl.net.api.MessageEncoderDecoder;
@@ -58,3 +59,75 @@ public class BlockingConnectionHandler<T> implements Runnable, ConnectionHandler
         //IMPLEMENT IF NEEDED
     }
 }
+=======
+package bgu.spl.net.srv;
+
+import bgu.spl.net.api.MessageEncoderDecoder;
+import bgu.spl.net.api.MessagingProtocol;
+import bgu.spl.net.api.StompMessagingProtocol;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class BlockingConnectionHandler<T> implements Runnable, ConnectionHandler<T> {
+
+    private final StompMessagingProtocol<T> protocol;
+    private final MessageEncoderDecoder<T> encdec;
+    private final Socket sock;
+    private BufferedInputStream in;
+    private BufferedOutputStream out;
+    private volatile boolean connected = true;
+
+    public BlockingConnectionHandler(Socket sock, MessageEncoderDecoder<T> reader, StompMessagingProtocol<T> protocol) {
+        this.sock = sock;
+        this.encdec = reader;
+        this.protocol = protocol;
+    }
+
+    @Override
+    public void run() {
+        try (Socket sock = this.sock) { //just for automatic closing
+            int read;
+
+            in = new BufferedInputStream(sock.getInputStream());
+            out = new BufferedOutputStream(sock.getOutputStream());
+
+            while (!protocol.shouldTerminate() && connected && (read = in.read()) >= 0) {
+                T nextMessage = encdec.decodeNextByte((byte) read);
+                //System.out.println("BlockingCH: nextMessage = " + nextMessage);
+                if (nextMessage != null) {
+                    protocol.process(nextMessage);
+                    if (protocol.shouldTerminate()) {
+                        close();
+                        break;
+                    }
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        connected = false;
+        sock.close();
+    }
+
+    @Override
+    public void send(T msg) {
+        try {
+            byte[] encodedMsg = encdec.encode(msg);
+            sock.getOutputStream().write(encodedMsg);
+            sock.getOutputStream().flush();
+            System.out.println("Attempting to send to client");
+        } catch (IOException e) {
+            System.err.println("Error Sending message to client: " + e.getMessage());
+            e.printStackTrace();
+            connected = false;
+        }
+    }
+}
+>>>>>>> be140d4... implemented ConnectionHandler
