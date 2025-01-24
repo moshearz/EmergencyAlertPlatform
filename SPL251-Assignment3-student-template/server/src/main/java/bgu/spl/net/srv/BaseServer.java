5f2e44b1b7b721e0ae903460f241e387936245ef
alonganot10 <alonganot@gmail.com>
2025-01-24 12:20:26 +0000

    implemented Server

--- SPL251-Assignment3-student-template/server/src/main/java/bgu/spl/net/srv/BaseServer.java
+++ SPL251-Assignment3-student-template/server/src/main/java/bgu/spl/net/srv/BaseServer.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package bgu.spl.net.srv;
 
 import bgu.spl.net.api.MessageEncoderDecoder;
@@ -59,3 +60,72 @@ public abstract class BaseServer<T> implements Server<T> {
     protected abstract void execute(BlockingConnectionHandler<T>  handler);
 
 }
+=======
+package bgu.spl.net.srv;
+
+import bgu.spl.net.api.MessageEncoderDecoder;
+import bgu.spl.net.api.MessagingProtocol;
+import bgu.spl.net.api.StompMessagingProtocol;
+import bgu.spl.net.impl.stomp.StompFrame;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.function.Supplier;
+
+public abstract class BaseServer<T> implements Server<T> {
+
+    private final int port;
+    private final Supplier<StompMessagingProtocol<T>> protocolFactory;
+    private final Supplier<MessageEncoderDecoder<T>> encdecFactory;
+    private final Connections<T> connections;
+    private ServerSocket sock;
+
+    public BaseServer(
+            int port,
+            Supplier<StompMessagingProtocol<T>> protocolFactory,
+            Supplier<MessageEncoderDecoder<T>> encdecFactory) {
+
+        this.port = port;
+        this.protocolFactory = protocolFactory;
+        this.encdecFactory = encdecFactory;
+        this.connections = new ConnectionsImpl<>();
+		this.sock = null;
+    }
+
+    @Override
+    public void serve() {
+
+        try (ServerSocket serverSock = new ServerSocket(port)) {
+			System.out.println("Server started");
+
+            this.sock = serverSock; //just to be able to close
+            
+
+            while (!Thread.currentThread().isInterrupted()) {
+                Socket clientSock = serverSock.accept();
+                StompMessagingProtocol<T> protocol = protocolFactory.get();
+                protocol.start(port, connections);
+                BlockingConnectionHandler<T> handler = new BlockingConnectionHandler<>(
+                    clientSock,
+                    encdecFactory.get(),
+                    protocol
+                );
+                connections.connect(handler);
+                execute(handler);
+            }
+        } catch (IOException ex) {
+        }
+
+        System.out.println("server closed!!!");
+    }
+
+    @Override
+    public void close() throws IOException {
+		if (sock != null)
+			sock.close();
+    }
+
+    protected abstract void execute(BlockingConnectionHandler<T>  handler);
+}
+>>>>>>> 50f41d5... implemented Server
