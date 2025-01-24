40a77326108619d7d4535fe3ec4d48bc91fab046
alonganot10 <alonganot@gmail.com>
2025-01-24 12:23:53 +0000

    Implemented main server function

--- SPL251-Assignment3-student-template/server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
+++ SPL251-Assignment3-student-template/server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package bgu.spl.net.impl.stomp;
 
 public class StompServer {
@@ -6,3 +7,35 @@ public class StompServer {
         // TODO: implement this
     }
 }
+=======
+package bgu.spl.net.impl.stomp;
+
+import bgu.spl.net.api.MessageEncoderDecoder;
+import bgu.spl.net.srv.Server;
+
+public class StompServer {
+
+    public static void main(String[] args) {
+        int port = Integer.parseInt(args[0]);
+        String serverType = args[1];
+
+        System.out.println("test");
+        if (serverType.equals("tpc")) {
+            Server.threadPerClient(
+                port, 
+                () -> new StompMessagingProtocolImpl(), 
+                () -> new StompFrameEncoderDecoder()
+            ).serve();
+        } else if (serverType.equals("reactor")) {
+            Server.reactor(
+                Runtime.getRuntime().availableProcessors(), 
+                port, 
+                () -> new StompMessagingProtocolImpl(), 
+                () -> new StompFrameEncoderDecoder()
+            ).serve();
+        } else {
+            System.out.println("Invalid server type. Acceptable types: 'tpc' , 'reactor'");
+        }
+    }
+}
+>>>>>>> dc50916... Implemented main server function
