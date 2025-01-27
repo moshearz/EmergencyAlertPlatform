5828b6037cf57981ab5514e8a6506ce7af7581ad
moshearz <moshearzuan1@gmail.com>
2025-01-27 20:04:31 +0000

    Final version of StompClient

--- SPL251-Assignment3-student-template/client/include/event.h
+++ SPL251-Assignment3-student-template/client/include/event.h
@@ -5,6 +5,9 @@
 #include <map>
 #include <vector>
 
+#include <sstream>
+
+
 class Event
 {
 private:
@@ -23,6 +26,7 @@ private:
     std::string eventOwnerUser;
 
 public:
+    void split_str(const std::string &str, char delimiter, std::vector<std::string> &output);
     Event(std::string channel_name, std::string city, std::string name, int date_time, std::string description, std::map<std::string, std::string> general_information);
     Event(const std::string & frame_body);
     virtual ~Event();
@@ -35,6 +39,15 @@ public:
     int get_date_time() const;
     const std::map<std::string, std::string> &get_general_information() const;
 };
+
+// Utility function to split a string by a delimiter
+inline void split_str(const std::string &str, char delimiter, std::vector<std::string> &result) {
+    std::stringstream ss(str);
+    std::string item;
+    while (std::getline(ss, item, delimiter)) {
+        result.push_back(item);
+    }
+}
 
 // an object that holds the names of the teams and a vector of events, to be returned by the parseEventsFile function
 struct names_and_events {
